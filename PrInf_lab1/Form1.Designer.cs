528bf9f2d287e7faa39bd6829bc7ad36bc0bc52f
Awsder BBM5 <awsd.andercen.022@gmail.com>
2023-04-21 00:31:30 +0300

    рп

--- PrInf_lab1/Form1.Designer.cs
+++ PrInf_lab1/Form1.Designer.cs
@@ -33,6 +33,8 @@
             textBox2 = new TextBox();
             textBox3 = new TextBox();
             button2 = new Button();
+            button3 = new Button();
+            label1 = new Label();
             SuspendLayout();
             // 
             // textBox1
@@ -41,7 +43,8 @@
             textBox1.Location = new Point(29, 30);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(486, 287);
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            textBox1.Size = new Size(587, 287);
             textBox1.TabIndex = 0;
             // 
             // button1
@@ -58,7 +61,7 @@
             // textBox2
             // 
             textBox2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox2.Location = new Point(547, 63);
+            textBox2.Location = new Point(622, 30);
             textBox2.Name = "textBox2";
             textBox2.Size = new Size(228, 29);
             textBox2.TabIndex = 2;
@@ -66,7 +69,7 @@
             // textBox3
             // 
             textBox3.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox3.Location = new Point(547, 121);
+            textBox3.Location = new Point(622, 65);
             textBox3.Name = "textBox3";
             textBox3.Size = new Size(228, 29);
             textBox3.TabIndex = 2;
@@ -74,7 +77,7 @@
             // button2
             // 
             button2.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(569, 175);
+            button2.Location = new Point(634, 100);
             button2.Name = "button2";
             button2.Size = new Size(193, 39);
             button2.TabIndex = 1;
@@ -82,11 +85,33 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += button2_Click;
             // 
+            // button3
+            // 
+            button3.Location = new Point(664, 188);
+            button3.Name = "button3";
+            button3.Size = new Size(126, 47);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            button3.Click += button3_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.Location = new Point(335, 356);
+            label1.Name = "label1";
+            label1.Size = new Size(68, 30);
+            label1.TabIndex = 4;
+            label1.Text = "label1";
+            // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(936, 513);
+            Controls.Add(label1);
+            Controls.Add(button3);
             Controls.Add(textBox3);
             Controls.Add(textBox2);
             Controls.Add(button2);
@@ -105,5 +130,7 @@
         private TextBox textBox2;
         private TextBox textBox3;
         private Button button2;
+        private Button button3;
+        private Label label1;
     }
 }
--- PrInf_lab1/Form1.cs
+++ PrInf_lab1/Form1.cs
@@ -8,29 +8,44 @@ namespace PrInf_lab1
     public partial class Form1 : Form
     {
         string data;
+        int f = 1;
+        bool stop = false;
+        List<string> listHash = new List<string>();
         public Form1()
         {
             InitializeComponent();
             DataFileGet();
+            label1.Text = "False";
+            label1.ForeColor = Color.Red;
         }
 
         private void DataFileGet()
         {
-            data = File.ReadAllText(@"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing1.txt");
+            data = File.ReadAllText(@"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing.txt");
+        }
+        private void DataFileGet(int i)
+        {
+            data = File.ReadAllText(@$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{i}.txt");
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             data = data.Replace(textBox2.Text, textBox3.Text);
-            byte[] bytes = Encoding.UTF8.GetBytes(data); 
-            string path = @"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing1.txt";
-            File.WriteAllBytes(path, bytes); 
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            string path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            File.WriteAllBytes(path, bytes);
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+            getHashSha1();
+        }
+
+        private void getHashSha1()
+        {
+
             string command = "openssl"; // им€ приложени€
-            string arguments = "dgst -sha1 C:\\VSProjects\\PrInf_lab1\\PrInf_lab1\\Resources\\leasing1.txt"; // аргументы
+            string arguments = $"dgst -sha1 C:\\VSProjects\\PrInf_lab1\\PrInf_lab1\\Resources\\leasing{f}.txt"; // аргументы
 
             ProcessStartInfo startInfo = new ProcessStartInfo();
             startInfo.FileName = command;
@@ -41,8 +56,257 @@ namespace PrInf_lab1
             Process process = new Process();
             process.StartInfo = startInfo;
             process.Start();
+            textBox1.Text = "";
             textBox1.Text = process.StandardOutput.ReadToEnd();
+            textBox1.Text += Environment.NewLine;
+            string a = textBox1.Text.Replace($"SHA1(C:\\VSProjects\\PrInf_lab1\\PrInf_lab1\\Resources\\leasing{f}.txt)= ", "").Replace("\r\n", "");
+            if (listHash.Contains(a))
+            {
+                textBox2.Text = $"Ќайдено совпадение - {a}{Environment.NewLine}";
+                stop = true;
+                label1.Text = "True";
+                label1.ForeColor = Color.Green;
+            }
+            listHash.Add(a);
             process.WaitForExit();
+
+            
         }
+
+        private async void button3_Click(object sender, EventArgs e)
+        {
+            DataFileGet();
+
+            byte[] bytes = Encoding.UTF8.GetBytes(data);
+            string path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            File.WriteAllBytes(path, bytes);
+            getHashSha1();
+            f++;
+
+            data = data.Replace("положени€", "услови€");
+            bytes = Encoding.UTF8.GetBytes(data);
+            path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            File.WriteAllBytes(path, bytes);
+            getHashSha1();
+            f++;
+            int temp = 0;
+            for (int i = 1; i < 2; i++)
+            {
+                DataFileGet(i); 
+
+                data = data.Replace("платежей", "выплат");
+                bytes = Encoding.UTF8.GetBytes(data);
+                path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+                File.WriteAllBytes(path, bytes);
+                getHashSha1();
+                f++;
+            }
+            temp = f - 1;
+            while (stop == false && f < 20000) 
+            { 
+                for (int i = temp / 2; i < temp; i++)
+                {
+                    DataFileGet(i);
+
+                    string[] words = data.Split(' ');
+                    data = String.Join(", ", words);
+                    bytes = Encoding.UTF8.GetBytes(data);
+                    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+                    File.WriteAllBytes(path, bytes);
+                    getHashSha1();
+                    f++;
+                }
+                temp = f - 1;
+                for (int i = temp / 2; i < temp; i++)
+                {
+                    DataFileGet(i);
+
+                    string[] words = data.Split(' ');
+                    data = String.Join(" однако ", words);
+                    bytes = Encoding.UTF8.GetBytes(data);
+                    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+                    File.WriteAllBytes(path, bytes);
+                    getHashSha1();
+                    f++;
+                }
+                temp = f - 1;
+                //for (int i = temp / 2; i < temp; i++)
+                //{
+                //    DataFileGet(i);
+
+                //    data = data.Replace(" ", " \b ");
+                //    bytes = Encoding.UTF8.GetBytes(data);
+                //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+                //    File.WriteAllBytes(path, bytes);
+                //    getHashSha1();
+                //    f++;
+                //}
+                //temp = f - 1;
+                //for (int i = temp / 2; i < temp; i++)
+                //{
+                //    DataFileGet(i);
+
+                //    data += "\r\n";
+                //    bytes = Encoding.UTF8.GetBytes(data);
+                //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+                //    File.WriteAllBytes(path, bytes);
+                //    getHashSha1();
+                //    f++;
+                //}
+                await Task.Delay(1);
+            }
+
+
+            //int temp = 0;
+            //for (int i = 1; i < 2; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("платежей", "выплат");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("экономики", "экономического развити€");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("–оссийской ‘едерации", "–оссии");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+
+            //    f++;
+            //}
+            //temp = f - 1;
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("млн", "миллионов");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+
+            //    f++;
+            //}
+            //temp = f - 1;
+
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("руб", "рублей");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+
+            //    f++;
+            //}
+            //temp = f - 1;
+
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("2", "6");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("лизинговых", "арендных");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("8", "4");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("96", "40 ");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data += "\r\n";
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+            //for (int i = temp / 2; i < temp; i++)
+            //{
+            //    DataFileGet(i);
+
+            //    data = data.Replace("договора", "соглашени€");
+            //    bytes = Encoding.UTF8.GetBytes(data);
+            //    path = @$"C:\VSProjects\PrInf_lab1\PrInf_lab1\Resources\leasing{f}.txt";
+            //    File.WriteAllBytes(path, bytes);
+            //    getHashSha1();
+            //    f++;
+            //}
+            //temp = f - 1;
+
+
+            string allItems = String.Join(Environment.NewLine, listHash);
+            textBox1.Text = allItems;
+            //for (int i = 0; i < listHash.Count; i++)
+            //{
+            //    textBox1.Text += listHash[i] + Environment.NewLine;
+            //}
+        }
+
     }
 }
